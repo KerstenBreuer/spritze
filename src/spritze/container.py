b97df75ff18c453262bf34ac1f6bcc32051f294a
Kersten Henrik Breuer <kersten-breuer@outlook.com>
2025-02-12 21:30:32 +0000

    extract DIRecord into dedicated DISpec

--- src/spritze/container.py
+++ src/spritze/container.py
@@ -1,10 +1,17 @@
 """Mock up of a container"""
 
 from abc import ABC
-from collections.abc import Sequence
-from dataclasses import dataclass
-from types import FunctionType, ModuleType
 from typing import Callable
+
+from .spec import (
+    AsyncContextProvider,
+    AsyncProvider,
+    Condition,
+    ContextProvider,
+    DISpec,
+    LiteralProvider,
+    Provider,
+)
 
 
 class DIMatcher(ABC):  # noqa: B024
@@ -15,25 +22,12 @@ class DIMatcher(ABC):  # noqa: B024
     pass
 
 
-@dataclass
-class DIRecord:
-    """A record of a dependency injection."""
-
-    constructor: Callable
-    singleton: bool = False
-    # Matching information
-    match_type: type | None = None
-    match_names: Sequence[str] | None = None
-    match_module: ModuleType | None = None
-    match_target: FunctionType | type | None = None
-    priority: int = 0
-
-
 class DIContext:
     """A resolver for dependency injection records active in a specific context."""
 
-    def __init__(self, records: Sequence[DIRecord], matcher: DIMatcher):
-        self.records = records
+    def __init__(self, spec: DISpec, matcher: DIMatcher):
+        self.spec = spec
+        self.matcher = matcher
 
     def resolve(self, subject: Callable) -> object:
         """Resolves a callable by automatically constructing and wiring all its
@@ -42,57 +36,38 @@ class DIContext:
         raise NotImplementedError
 
 
-class DIContainer:
-    """A container for dependency injection records."""
+class GeneralDIContainer:
+    """A general-purpose DI Container."""
 
-    def __init__(self):
-        self.records: list
-
-    def add(  # noqa: PLR0913
+    def __init__(
         self,
-        constructor: Callable,
-        *,
-        singleton: bool = False,
-        match_type: type | None = None,
-        match_names: Sequence[str] | None = None,
-        match_module: ModuleType | None = None,
-        match_target: FunctionType | type | None = None,
-        priority: int = 0,
-    ) -> None:
-        """Add a record to the container."""
-        record = DIRecord(
-            constructor=constructor,
-            singleton=singleton,
-            match_type=match_type,
-            match_names=match_names,
-            match_module=match_module,
-            match_target=match_target,
-            priority=priority,  # priority might control the order of inclusion in the records
-        )
-        self.records.append(record)
+        map: dict[
+            Condition,
+            Provider
+            | AsyncProvider
+            | ContextProvider
+            | AsyncContextProvider
+            | LiteralProvider,
+        ],
+    ):
+        self.spec = DISpec(map)
 
-    def add_literal(  # noqa: PLR0913
+    def add(
         self,
-        literal: object,
-        *,
-        singleton: bool = False,
-        match_type: type | None = None,
-        match_names: Sequence[str] | None = None,
-        match_module: ModuleType | None = None,
-        match_target: FunctionType | type | None = None,
-        priority: int = 0,
-    ) -> None:
-        """Add a literal to the container."""
-        self.add(
-            constructor=lambda: literal,
-            singleton=singleton,
-            match_type=match_type,
-            match_names=match_names,
-            match_module=match_module,
-            match_target=match_target,
-            priority=priority,
-        )
+        condition: Condition,
+        provider: Provider
+        | AsyncProvider
+        | ContextProvider
+        | AsyncContextProvider
+        | LiteralProvider,
+    ):
+        """Add a condition-provider pair to the spec."""
+        self.spec.add(condition=condition, provider=provider)
 
     def __enter__(self) -> DIContext:
         """Create a new DIContext scoped toward a with block."""
+        raise NotImplementedError
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Clean up the DIContext."""
         raise NotImplementedError
---
+++ src/spritze/spec.py
@@ -0,0 +1,130 @@
+"""Structures to specify Conditions for matching ParameterDescriptions to
+a Provider.
+"""
+
+from collections.abc import Awaitable, Callable, Sequence
+from contextlib import (
+    AbstractAsyncContextManager,
+    AbstractContextManager,
+)
+from dataclasses import dataclass
+from enum import Enum
+from types import FunctionType, ModuleType
+
+
+class ConditionParams(Enum):
+    """The parameters that can be used in a Condition."""
+
+    TYPE_ = "type_"
+    LABELS = "labels"
+    TAGS = "tags"
+    MODULE = "module"
+    TARGET = "target"
+
+
+@dataclass
+class Condition:
+    """Specification of conditions to match a ParameterDescription."""
+
+    type_: type | None = None
+    labels: Sequence[str] | None = None
+    tags: Sequence[str] | None = None
+    module: ModuleType | None = None
+    target: FunctionType | type | None = None
+
+
+@dataclass
+class Provider[D]:
+    """Instructions for constructing a dependency based on a simple callable."""
+
+    constructor: Callable[..., D]
+    as_singelton: bool = False
+
+
+@dataclass
+class AsyncProvider[D]:
+    """Instructions for constructing a dependency based on a coroutine."""
+
+    constructor: Callable[..., Awaitable[D]]
+    as_singelton: bool = False
+
+
+@dataclass
+class ContextProvider[D]:
+    """Instructions for constructing a dependency based on a context manager."""
+
+    constructor: Callable[..., AbstractContextManager[D]]
+    as_singelton: bool = False
+
+
+@dataclass
+class AsyncContextProvider[D]:
+    """Instructions for constructing a dependency based on an async context manager."""
+
+    constructor: Callable[..., AbstractAsyncContextManager[D]]
+    as_singelton: bool = False
+
+
+class LiteralProvider:
+    """Description of a object that is passed "as is". Hence this is not a
+    constructor for a dependency this is the already constructed dependency itself.
+    """
+
+    def __init__(self, literal: object):
+        self.literal = literal
+
+    def __repr__(self):  # noqa: D105
+        return f"LiteralProvider({self.literal})"
+
+    def __str__(self):  # noqa: D105
+        return repr(self)
+
+
+class DISpec:
+    """A specification that describes how Conditions (for matching
+    ParametersDescriptions) are associated to Providers (to provide instances of the
+    required dependencies).
+    """
+
+    def __init__(
+        self,
+        map: dict[
+            Condition,
+            Provider
+            | AsyncProvider
+            | ContextProvider
+            | AsyncContextProvider
+            | LiteralProvider,
+        ],
+    ):
+        self.map = map
+
+    def add(
+        self,
+        condition: Condition,
+        provider: Provider
+        | AsyncProvider
+        | ContextProvider
+        | AsyncContextProvider
+        | LiteralProvider,
+    ):
+        """Add a condition-provider pair to the spec."""
+        self.map[condition] = provider
+
+    def used_condition_params(self) -> set[ConditionParams]:
+        """Summarizes which parameters are used in the contained Conditions."""
+        return {
+            param
+            for param in ConditionParams
+            if any(getattr(cond, param.value) is not None for cond in self.map)
+        }
+
+    def used_providers(self) -> set[type]:
+        """Summarizes which types of providers are used in the contained Providers."""
+        return {type(provider) for provider in self.map.values()}
+
+    def __repr__(self):  # noqa: D105
+        return f"DISpec({self.map})"
+
+    def __str__(self):  # noqa: D105
+        return repr(self)
